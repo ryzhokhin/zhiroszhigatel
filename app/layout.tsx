8c86d1139862d2159f2140ce2ccdd7db45875430
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2024-09-10 12:19:36 -0700

    sixth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Telegram Mini App",
-  description: "App for zhiroszhigatel",
+  title: 'Telegram Mini App',
+  description: 'Telegram mini app for zhiroszhigatel'
 };
 
 export default function RootLayout({
@@ -26,14 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive"/>
-    </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
